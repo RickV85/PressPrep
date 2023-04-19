d0a40cb432c3baf3b620e79637591f62f675241e
Rick Vermeil <rickv85@gmail.com>
2023-04-19 11:22:55 -0600

    Add link to site name and style Nav

--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -1,10 +1,13 @@
 import './Nav.css'
+import { NavLink } from 'react-router-dom'
 
 export default function Nav() {
 
   return (
     <nav className='navbar'>
-      <h1>PrepPress</h1>
+      <NavLink to={"/"} className="home-link">
+        <h1>PrepPress</h1>
+      </NavLink>
       <hr className='nav-divider' />
     </nav>
   )
---
+++ src/Containers/ArticleView/ArticleView.tsx
@@ -0,0 +1,11 @@
+import Nav from "../../Components/Nav/Nav"
+
+export default function ArticleView() {
+  
+
+  return (
+    <main>
+      <Nav />
+    </main>
+  )
+}
