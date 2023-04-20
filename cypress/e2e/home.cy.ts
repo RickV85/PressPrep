7931e67818e32e71e30ad4c4ce72b773879abe35
Rick Vermeil <rickv85@gmail.com>
2023-04-20 11:25:04 -0600

    Add initial cypress test and intercept

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,7 +1,14 @@
 /// <reference types="cypress" />
 
 describe('home', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+  beforeEach(() => {
+    cy.intercept(`https://api.nytimes.com/svc/topstories/v2/*`, {
+      fixture: 'newsResults.json'
+    })
+    cy.visit('http://localhost:5173/')
+  })
+
+  it('Should show the loading message before results load', () => {
+    cy.get('.loading').contains('loading')
   })
 })
