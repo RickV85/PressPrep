2f4f5d36061b221920e59e233092316c862e9d1d
Rick Vermeil <rickv85@gmail.com>
2023-04-20 11:46:11 -0600

    Add tests for dropdown and results

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -3,12 +3,31 @@
 describe('home', () => {
   beforeEach(() => {
     cy.intercept(`https://api.nytimes.com/svc/topstories/v2/*`, {
-      fixture: 'newsResults.json'
+      fixture: 'homeResults.json'
     })
     cy.visit('http://localhost:5173/')
   })
 
-  it('Should show the loading message before results load', () => {
-    cy.get('.loading').contains('loading')
+  it('should show the site title', () => {
+    cy.get('h1').should('be.visible')
+  })
+
+  it('should show news selector dropdown', () => {
+    cy.get('.news-dropdown').should('be.visible')
+  })
+
+  it('should update the search field value when an option is picked and show appropriate results', () => {
+    cy.intercept(`https://api.nytimes.com/svc/topstories/v2/*`, {
+      fixture: 'foodResults.json'
+    })
+    cy.get('.news-dropdown').select('Food').should('have.value', 'food')
+    cy.get('.tile-title').eq(0).contains('Barbara Lynch, a Leading Boston Restaurateur, Is Accused of Workplace Abuse')
+  })
+
+  it('should show the error message if there is an issue fetching articles', () => {
+    cy.intercept(`https://api.nytimes.com/svc/topstories/v2/*`, {
+      statusCode: 500,
+    })
+    cy.get('.loading').contains('An error occurred')
   })
 })
